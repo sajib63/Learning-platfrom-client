caef9c2dd1c73bb385fb449d6c664bac4809b389
Sajib Ahmed <sajib7315@gmail.com>
2022-11-09 12:40:38 +0600

    adding navbar

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             
         </div>
     );
