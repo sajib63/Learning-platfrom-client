5ff0aba1f2d94a5bb9978a09df395b7001f7596e
Sajib Ahmed <sajib7315@gmail.com>
2022-11-09 13:30:04 +0600

    adding login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './Components/Router';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="container mx-auto">
       <RouterProvider router={router}></RouterProvider>
+      <Toaster />
     </div>
   );
 }
